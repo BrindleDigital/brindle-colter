1b2e5ed6adad3549d832d06e1510c03d326373fe
David Faltermier <david@codegeek.net>
2022-09-04 17:22:55 -0600

    Create 404/500 pages

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CT;
+
+?>
+
+<?php get_header(); ?>
+
+<div class="ct-error-container">
+    <h1>Oh, no!</h1>
+    <p>The page you're looking for is not here.</p>
+
+    <div class="wp-block-button is-style-fill">
+        <a class="wp-block-button__link" href="/" style="border-radius:0px">Go Home</a>
+    </div>
+</div>
+
+<?php get_footer(); ?>
---
+++ 500.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CT;
+
+?>
+
+<?php get_header(); ?>
+
+<div class="ct-error-container">
+    <h1>Oh no!</h1>
+    <h2>Looks like our server is troubled.</h2>
+    <p>Please try again.</p>
+</div>
+
+<?php get_footer(); ?>
